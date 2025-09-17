7335ccd305b61c4a92143b5a42f806bc0c37f83c
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-09-17 12:47:19 +0100

    update code - frame and fonts change according to user inputs

--- watermarker_gui_modern.py
+++ watermarker_gui_modern.py
@@ -4,23 +4,35 @@ from tkinter import *
 from tkinter import colorchooser
 import tkinter.font as tkFont
 
+
 # set the default color theme for the whole app
 ctk.set_default_color_theme('autumn.json')
 # appearance available system/light/dark
-ctk.set_appearance_mode('system')
+ctk.set_appearance_mode('dark')
 # define the app size
 app_w, app_h = 1200, 800
 
 class Watermarker_App(ctk.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         # app size
         self.geometry(f'{app_w}x{app_h}')
         self.title('Image Watermarking App')
 
+        # frame for control panel
+        self.control_panel_frame = ctk.CTkFrame(
+            master=self,
+            width=200,
+            height=800,
+            corner_radius=5,
+            border_width=2
+        )
+        self.control_panel_frame.pack(pady=20, padx=20, fill='y', side='right')
+
         # text label
         self.textLabel = ctk.CTkLabel(
-            self,
+            self.control_panel_frame,
             text='Watermark text: '
         )
         self.textLabel.grid(
@@ -30,39 +42,44 @@ class Watermarker_App(ctk.CTk):
             pady=20,
             sticky='ew'
         )
-        # Name Entry Field
+        # add frame for text box
+        self.text_frame = ctk.CTkScrollableFrame(
+            self.control_panel_frame,
+            orientation='horizontal',
+            width=180,
+            height=50,
+            label_text='Enter text'
+            )
+        self.text_frame.grid(row=0, column=1, pady=20, padx=20)
+
+        # Entry text box
         self.textEntry_var = tk.StringVar()
-        self.textEntry = ctk.CTkEntry(
-            self,
-            placeholder_text="Enter text"
+        self.textEntry = tk.Text(
+            self.text_frame
         )
         self.textEntry.grid(
-            row=0,
-            column=1,
-            columnspan=5,
-            padx=20,
-            pady=20,
+            row=0, column=0,
             sticky="ew"
         )
 
 
         # font types
         self.fontLabel = ctk.CTkLabel(
-            self,
+            self.control_panel_frame,
             text='Font: '
         )
         self.fontLabel.grid(
-            row=1,
+            row=3,
             column=0,
             padx=20,
             pady=20,
             sticky='ew'
         )
-        fonts = sorted(tkFont.families())  # get system fonts
+        fonts = sorted(tkFont.families())
         # font dropdown
         self.font_var = tk.StringVar(value="Arial")  # default font
         self.font_menu = ctk.CTkOptionMenu(
-            self,
+            self.control_panel_frame,
             values=fonts,
             variable=self.font_var,
             command=self.update_font  # called when user selects a font
@@ -77,26 +94,28 @@ class Watermarker_App(ctk.CTk):
 
 
         # font_size
+        self.font_size_var = tk.IntVar(value=10)
         self.font_sizeLabel = ctk.CTkLabel(
-             self,
+             self.control_panel_frame,
              text='Choose font size: '
         )
         self.font_sizeLabel.grid(
-            row=2,
+            row=4,
             column=0,
             padx=20,
             pady=20,
             sticky='ew'
         )
+        self.font_size_var.trace_add("write", lambda *args: self.update_font())
         self.font_size_spinbox = tk.Spinbox(
-            self,
+            self.control_panel_frame,
             from_=5,
             to=30,
             width=5,
-            command=self.update_font
+            textvariable=self.font_size_var
             )
         self.font_size_spinbox.grid(
-            row=2,
+            row=4,
             column=1,
             padx=20,
             pady=20,
@@ -104,48 +123,54 @@ class Watermarker_App(ctk.CTk):
         )
 
         # user to choose a font color
-        self.color_button_var = tk.StringVar(value="white")  # default font
+        self.color_button_var = tk.StringVar(value="black")  # default font
         self.color_buttonLabel = ctk.CTkLabel(
-            self,
-            text='Choose font color: ',
-            variable=self.color_button_var
+            self.control_panel_frame,
+            text='Choose font color: '
         )
         self.color_buttonLabel.grid(
-            row=3,
+            row=5,
             column=0,
             padx=20,
             pady=20,
             sticky='ew'
         )
         self.color_button = tk.Button(
-            self,
+            self.control_panel_frame,
             text='Select',
             command = self.update_color
         )
         self.color_button.grid(
-            row=3,
+            row=5,
             column=1,
             padx=20,
             pady=20,
             sticky='ew'
         )
 
+    # get available fonts
+
+
     # update font according to the user input
     def update_font(self, choice=None):
-        font_name = self.font_var.get()
-        font_size = int(self.font_size_spinbox.get())
-        self.textEntry.config(font=(font_name, font_size))
+        """update font type and size according to the user's choice"""
+        font_type = self.font_var.get()
+        font_size = int(self.font_size_var.get())
+        font_color = self.color_button_var.get()
+        self.textEntry.config(font=(font_type, font_size), fg=font_color)
 
     # update font color according to the user input
     def update_color(self):
+        """Update font color according to the user's choice"""
         color_code = colorchooser.askcolor(title='Choose font color')
         if color_code[1]:  # hex colour
+            self.color_button_var.set(color_code[1])
             self.color_button.config(
                             text=color_code[1],
                             bg=color_code[1],
                             fg='black' if color_code[1].lower() != "#ffffff" else "black"
                             )
-
+            self.update_font()
 
 
 
