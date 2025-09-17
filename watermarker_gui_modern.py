f2b84d1dae634d1aff367f494c2e4040d69219dd
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-09-17 15:13:57 +0100

    update on color and transparency

--- watermarker_gui_modern.py
+++ watermarker_gui_modern.py
@@ -3,6 +3,7 @@ import tkinter as tk
 from tkinter import *
 from tkinter import colorchooser
 import tkinter.font as tkFont
+from PIL import ImageColor
 
 
 # set the default color theme for the whole app
@@ -10,7 +11,7 @@ ctk.set_default_color_theme('autumn.json')
 # appearance available system/light/dark
 ctk.set_appearance_mode('dark')
 # define the app size
-app_w, app_h = 1200, 800
+app_w, app_h = 1300, 800
 
 class Watermarker_App(ctk.CTk):
     def __init__(self, *args, **kwargs):
@@ -20,6 +21,19 @@ class Watermarker_App(ctk.CTk):
         self.geometry(f'{app_w}x{app_h}')
         self.title('Image Watermarking App')
 
+        # picture frame
+        self.picture_frame = ctk.CTkFrame(
+            master=self,
+            width=900,
+            height=800,
+            corner_radius=5,
+            border_width=2
+        )
+        self.picture_frame.pack(pady=10, padx=5, fill='y', side='left')
+
+
+
+
         # frame for control panel
         self.control_panel_frame = ctk.CTkFrame(
             master=self,
@@ -28,7 +42,7 @@ class Watermarker_App(ctk.CTk):
             corner_radius=5,
             border_width=2
         )
-        self.control_panel_frame.pack(pady=20, padx=20, fill='y', side='right')
+        self.control_panel_frame.pack(pady=10, padx=5, fill='y', side='right')
 
         # text label
         self.textLabel = ctk.CTkLabel(
@@ -66,7 +80,7 @@ class Watermarker_App(ctk.CTk):
         # font types
         self.fontLabel = ctk.CTkLabel(
             self.control_panel_frame,
-            text='Font: '
+            text='Font style: '
         )
         self.fontLabel.grid(
             row=3,
@@ -97,7 +111,7 @@ class Watermarker_App(ctk.CTk):
         self.font_size_var = tk.IntVar(value=10)
         self.font_sizeLabel = ctk.CTkLabel(
              self.control_panel_frame,
-             text='Choose font size: '
+             text='Font size: '
         )
         self.font_sizeLabel.grid(
             row=4,
@@ -123,24 +137,24 @@ class Watermarker_App(ctk.CTk):
         )
 
         # user to choose a font color
-        self.color_button_var = tk.StringVar(value="black")  # default font
-        self.color_buttonLabel = ctk.CTkLabel(
+        self.font_color_var = tk.StringVar(value="black")  # default font
+        self.font_colorLabel = ctk.CTkLabel(
             self.control_panel_frame,
-            text='Choose font color: '
+            text='Font color: '
         )
-        self.color_buttonLabel.grid(
+        self.font_colorLabel.grid(
             row=5,
             column=0,
             padx=20,
             pady=20,
             sticky='ew'
         )
-        self.color_button = tk.Button(
+        self.font_color = tk.Button(
             self.control_panel_frame,
             text='Select',
             command = self.update_color
         )
-        self.color_button.grid(
+        self.font_color.grid(
             row=5,
             column=1,
             padx=20,
@@ -148,15 +162,42 @@ class Watermarker_App(ctk.CTk):
             sticky='ew'
         )
 
-    # get available fonts
-
+        # apply transparency to font
+        self.font_transparency_slider_var = tk.IntVar(value=100)  # default transparency
+        self.font_transparency_sliderLabel = ctk.CTkLabel(
+            self.control_panel_frame,
+            text='Transparency (0-100%): '
+        )
+        self.font_transparency_sliderLabel.grid(
+            row=6,
+            column=0,
+            padx=20,
+            pady=20,
+            sticky='ew'
+        )
+        self.font_transparency_slider = ctk.CTkSlider(
+            self.control_panel_frame,
+            from_=0,
+            to=100,
+            command=self.update_transparency
+        )
+        self.font_transparency_slider.grid(
+            row=6,
+            column=1,
+            padx=20,
+            pady=20,
+            sticky='ew'
+        )
 
     # update font according to the user input
-    def update_font(self, choice=None):
+    def update_font(self):
         """update font type and size according to the user's choice"""
-        font_type = self.font_var.get()
-        font_size = int(self.font_size_var.get())
-        font_color = self.color_button_var.get()
+        font_type = self.font_var.get() if hasattr(self, "font_var") else "Arial"
+        try:
+            font_size = int(self.font_size_var.get())
+        except (ValueError, TypeError):
+            font_size = 12 # detault
+        font_color = self.font_color_var.get() if hasattr(self, "font_color_var") else "black"
         self.textEntry.config(font=(font_type, font_size), fg=font_color)
 
     # update font color according to the user input
@@ -164,13 +205,16 @@ class Watermarker_App(ctk.CTk):
         """Update font color according to the user's choice"""
         color_code = colorchooser.askcolor(title='Choose font color')
         if color_code[1]:  # hex colour
-            self.color_button_var.set(color_code[1])
-            self.color_button.config(
+            self.font_color_var.set(color_code[1])
+            self.font_color.config(
                             text=color_code[1],
                             bg=color_code[1],
                             fg='black' if color_code[1].lower() != "#ffffff" else "black"
                             )
             self.update_font()
+
+    def update_transparency(self, value):
+        self.font_transparency_slider_var = int(float(value) * 255 / 100)
 
 
 
