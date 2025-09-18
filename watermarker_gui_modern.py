0838fd78beee5dc0f7dec0debf2848f7fc5ff071
Kaho Legradi <legradi.kaho+github@gmail.com>
2025-09-18 12:29:24 +0100

    update on color, font, styles, canvas, frames

--- watermarker_gui_modern.py
+++ watermarker_gui_modern.py
@@ -3,7 +3,9 @@ import tkinter as tk
 from tkinter import *
 from tkinter import colorchooser
 import tkinter.font as tkFont
-from PIL import ImageColor
+from tkinter import filedialog, messagebox
+from PIL import Image, ImageTk, ImageFont, ImageDraw, ImageColor
+
 
 
 # set the default color theme for the whole app
@@ -11,7 +13,8 @@ ctk.set_default_color_theme('autumn.json')
 # appearance available system/light/dark
 ctk.set_appearance_mode('dark')
 # define the app size
-app_w, app_h = 1300, 800
+app_w, app_h = 1360, 800
+pic_frame_w, pic_frame_h = 900, 800
 
 class Watermarker_App(ctk.CTk):
     def __init__(self, *args, **kwargs):
@@ -24,14 +27,50 @@ class Watermarker_App(ctk.CTk):
         # picture frame
         self.picture_frame = ctk.CTkFrame(
             master=self,
-            width=900,
-            height=800,
+            width=pic_frame_w,
+            height=pic_frame_h,
             corner_radius=5,
             border_width=2
         )
         self.picture_frame.pack(pady=10, padx=5, fill='y', side='left')
 
+        # stop shrinking to fit contents
+        self.picture_frame.pack_propagate(False)
 
+        # get a correct color depending on the setting
+        frame_color = self.picture_frame.cget("fg_color")
+        # get a screen mode
+        screen_mode = ctk.get_appearance_mode().lower()
+        screen_color = ''
+        # determine a valid color string
+        if isinstance(frame_color, list):
+            screen_color = frame_color[0] if screen_mode == "light" else frame_color[1]
+        else:
+            screen_color = frame_color  # already a single color string
+        # setting a canvas
+        self.pic_canvas = tk.Canvas(
+            self.picture_frame,
+            width=pic_frame_w,
+            height=pic_frame_h,
+            highlightthickness=0,             # remove border
+            bd=0,                             # no 3D border
+            bg=screen_color  # match CTkFrame background
+        )
+        self.pic_canvas.pack(fill="both", expand=True, padx=10, pady=10)
+
+        self.upload_button = ctk.CTkButton(
+            self.picture_frame,
+            text='Upload picture',
+            height=100,
+            width=100,
+            font=('Ariel',24),
+            hover=True,
+            hover_color='grey',
+            corner_radius=10,
+            command=self.upload_pic
+        )
+        # show a button at the centre of pic frame
+        self.upload_button.place(relx=0.5, rely=0.5, anchor="center")
 
 
         # frame for control panel
@@ -43,6 +82,9 @@ class Watermarker_App(ctk.CTk):
             border_width=2
         )
         self.control_panel_frame.pack(pady=10, padx=5, fill='y', side='right')
+
+        # stop shrinking to fit contents
+        self.control_panel_frame.pack_propagate(False)
 
         # text label
         self.textLabel = ctk.CTkLabel(
@@ -61,8 +103,7 @@ class Watermarker_App(ctk.CTk):
             self.control_panel_frame,
             orientation='horizontal',
             width=180,
-            height=50,
-            label_text='Enter text'
+            height=50
             )
         self.text_frame.grid(row=0, column=1, pady=20, padx=20)
 
@@ -91,7 +132,7 @@ class Watermarker_App(ctk.CTk):
         )
         fonts = sorted(tkFont.families())
         # font dropdown
-        self.font_var = tk.StringVar(value="Arial")  # default font
+        self.font_var = tk.StringVar(value="DejaVu Serif")  # default font
         self.font_menu = ctk.CTkOptionMenu(
             self.control_panel_frame,
             values=fonts,
@@ -184,18 +225,39 @@ class Watermarker_App(ctk.CTk):
         self.font_transparency_slider.grid(
             row=6,
             column=1,
-            padx=20,
-            pady=20,
+            padx=5,
+            pady=5,
             sticky='ew'
         )
 
+    # Upload pic
+    def upload_pic(self):
+        fileTypes = [("Image Files", "*.jpg *.jpeg *.png")]
+        path = tk.filedialog.askopenfilename(filetypes=fileTypes)
+        if len(path):
+            self.image_path = path
+            self.img = Image.open(path)
+            img_w, img_h = self.img.size
+            if img_w > pic_frame_w and img_h> pic_frame_h:
+                self.img.thumbnail((pic_frame_w-20, pic_frame_h-20), Image.LANCZOS)
+            pic = ImageTk.PhotoImage(self.img)
+
+            # hide the upload button
+            self.upload_button.place_forget()
+
+            self.pic_canvas.delete('all')
+            self.pic_canvas.create_image(0, 0, image=pic, anchor='nw')
+            self.pic_canvas.image = pic
+        else:
+            print('No file is selected')
+
     # update font according to the user input
-    def update_font(self):
+    def update_font(self, value=None):
         """update font type and size according to the user's choice"""
         font_type = self.font_var.get() if hasattr(self, "font_var") else "Arial"
         try:
             font_size = int(self.font_size_var.get())
-        except (ValueError, TypeError):
+        except (ValueError, TypeError, TclError):
             font_size = 12 # detault
         font_color = self.font_color_var.get() if hasattr(self, "font_color_var") else "black"
         self.textEntry.config(font=(font_type, font_size), fg=font_color)
